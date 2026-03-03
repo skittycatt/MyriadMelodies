81a524da1ca31e000947c5c33277ae0c7d1ef2c8
Jade <jdenwhitney@gmail.com>
2026-03-02 18:30:12 -0600

    Removed php file, data retrieval working now through corsproxy.io

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  $("#search-form").submit(function (event) {
+  $("#search-form").submit(async function (event) {
     event.preventDefault();
-    $.get("get_data.php", { url: "https://sg-hk4e-api.hoyoverse.com/event/musicugc/v1/work_detail?lang=en-us&game_biz=hk4e_global&is_mobile=false&region=os_usa&share_code=" + document.querySelector("#search-id").value.trim() })
-      .done(function (response) {
-        console.log(response);
-      });
+    const url = "https://corsproxy.io/?url=" + encodeURIComponent("https://sg-hk4e-api.hoyoverse.com/event/musicugc/v1/work_detail?lang=en-us&game_biz=hk4e_global&is_mobile=false&region=os_usa&share_code="
+      + document.querySelector("#search-id").value.trim());
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const result = await response.json();
+      console.log(result);
+      const mapper = result.data.work.user.nickname;
+      const game_data = result.data.work.game_data;
+      console.log("map: " + game_data.name)
+      console.log("mapper: " + mapper);
+      console.log("note count: " + game_data.music_cnt);
+    }
+    catch (error) {
+      console.error(error.message);
+    }
+
   });
 });
--- get_data.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-    header('Access-Control-Allow-Methods: GET, POST');
-    $url = $_GET["url"];
-    echo file_get_contents($url);
-?>
