7643502d09c8149d52f9a8ef412c260c88e1e126
Exploshe <stevenluu10@gmail.com>
2026-03-10 14:58:45 -0700

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
       const mapper = result.data.work.user.nickname;
       const game_data = result.data.work.game_data;
       document.querySelector(".info").innerHTML = `map: ${game_data.name}<br>mapper: ${mapper}<br>note count: ${game_data.music_cnt}`;
+      const r = await fetch("http://34.72.70.49:3000");
+      const r2 = await r.text();
+      console.log(r2);
     }
     catch (error) {
       console.error(error.message);
